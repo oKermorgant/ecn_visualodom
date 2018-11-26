68fc17c4efab65a306407f3bb938d686bc9c1851
Olivier Kermorgant <olivier_kermorgant@yahoo.fr>
2018-11-26 08:20:32 +0100

    does nothing if no matches

--- include/ecn_visualodom/ar_display.h
+++ include/ecn_visualodom/ar_display.h
@@ -12,91 +12,94 @@
 class ModelDisplay
 {
 public:
-    ModelDisplay(bool use_ar = false) {init_ = false;use_ar_ = use_ar;}
+  ModelDisplay(bool use_ar = false) {init_ = false;use_ar_ = use_ar;}
 
-    void init(const vpCameraParameters &_cam, vpImage<vpRGBa> &_I)
+  void init(const vpCameraParameters &_cam, vpImage<vpRGBa> &_I)
+  {
+    init_ = true;
+    cam_ = _cam;
+
+    if(!use_ar_)
     {
-        init_ = true;
-        cam_ = _cam;
-
-        if(!use_ar_)
-        {
-            display_.init(_I, -1, -1, "Pose display");
-            return;
-        }
-
-        ogre_ =    vpAROgre(_cam, _I.getWidth(), _I.getHeight(), OGRE_RESSOURCE_PATH, OGRE_RESSOURCE_PATH);
-        ogre_.init(_I);
-        ogre_.load("Robot", "robot.mesh");
-        ogre_.setScale("Robot", 0.002f,0.002f,0.002f);
-        ogre_.setRotation("Robot", vpRotationMatrix(vpRxyzVector(-M_PI/2, -M_PI/2, 0)));
-        // Add an optional point light source
-        Ogre::Light * light = ogre_.getSceneManager()->createLight();
-        light->setDiffuseColour(1, 1, 1); // scaled RGB values
-        light->setSpecularColour(1, 1, 1); // scaled RGB values
-        light->setPosition(5, 5, -10);
-        light->setType(Ogre::Light::LT_POINT);
+      display_.init(_I, -1, -1, "Pose display");
+      return;
     }
 
-    void init(const vpCameraParameters &_cam, cv::Mat &_im)
+    ogre_ =    vpAROgre(_cam, _I.getWidth(), _I.getHeight(), OGRE_RESSOURCE_PATH, OGRE_RESSOURCE_PATH);
+    ogre_.addResource(std::string(OGRE_RESSOURCE_PATH) + "/media");
+    ogre_.addResource(std::string(OGRE_RESSOURCE_PATH) + "/media/models");
+    ogre_.addResource(std::string(OGRE_RESSOURCE_PATH) + "/media/materials/scripts");
+    ogre_.addResource(std::string(OGRE_RESSOURCE_PATH) + "/media/materials/textures");
+
+    ogre_.init(_I);
+    ogre_.load("Robot", "robot.mesh");
+    ogre_.setScale("Robot", 0.002f,0.002f,0.002f);
+    ogre_.setRotation("Robot", vpRotationMatrix(vpRxyzVector(-M_PI/2, -M_PI/2, 0)));
+    // Add an optional point light source
+    Ogre::Light * light = ogre_.getSceneManager()->createLight();
+    light->setDiffuseColour(1, 1, 1); // scaled RGB values
+    light->setSpecularColour(1, 1, 1); // scaled RGB values
+    light->setPosition(5, 5, -10);
+    light->setType(Ogre::Light::LT_POINT);
+  }
+
+  void init(const vpCameraParameters &_cam, cv::Mat &_im)
+  {
+    vpImageConvert::convert(_im, I_);
+    init(_cam, I_);
+  }
+
+  ModelDisplay(const vpCameraParameters &_cam, vpImage<vpRGBa> &_I, bool use_ar = false)
+  {
+    use_ar_ = use_ar;
+    init(_cam, _I);
+  }
+
+  // with opencv inputs
+  ModelDisplay(const vpCameraParameters &_cam, cv::Mat &_im, bool use_ar = false)
+  {
+    use_ar_ = use_ar;
+    init(_cam, _im);
+  }
+
+
+  inline bool continueRendering()
+  {
+    if(!init_ || !use_ar_) return true;
+    return ogre_.continueRendering();
+  }
+
+  inline void display(const vpImage<vpRGBa> &_I, const vpHomogeneousMatrix &_cMw)
+  {
+    if(!init_) return;
+
+    if(use_ar_)
     {
-        vpImageConvert::convert(_im, I_);
-        init(_cam, I_);
+      ogre_.display(_I, _cMw);
+      vpTime::wait(15);
+    }
+    else
+    {
+      vpDisplay::displayFrame(_I, _cMw, cam_, .1);
+      vpDisplay::flush(_I);
+      vpDisplay::display(_I);
     }
 
-    ModelDisplay(const vpCameraParameters &_cam, vpImage<vpRGBa> &_I, bool use_ar = false)
-    {
-        use_ar_ = use_ar;
-        init(_cam, _I);
-    }
-
-    // with opencv inputs
-    ModelDisplay(const vpCameraParameters &_cam, cv::Mat &_im, bool use_ar = false)
-    {
-        use_ar_ = use_ar;
-        init(_cam, _im);
-    }
+  }
 
 
-    inline bool continueRendering()
-    {
-        if(!init_ || !use_ar_) return true;
-        return ogre_.continueRendering();
-    }
-
-    inline void display(const vpImage<vpRGBa> &_I, const vpHomogeneousMatrix &_cMw)
-    {
-        if(!init_) return;
-
-        if(use_ar_)
-        {
-            ogre_.display(_I, _cMw);
-            vpTime::wait(15);
-        }
-        else
-        {
-            vpDisplay::displayFrame(_I, _cMw, cam_, .1);
-            vpDisplay::flush(_I);
-            vpDisplay::display(_I);
-        }
-
-    }
-
-
-    inline void display(cv::Mat &_im, const vpHomogeneousMatrix &_cMw)
-    {
-        vpImageConvert::convert(_im, I_);
-        display(I_, _cMw);
-    }
+  inline void display(cv::Mat &_im, const vpHomogeneousMatrix &_cMw)
+  {
+    vpImageConvert::convert(_im, I_);
+    display(I_, _cMw);
+  }
 
 protected:
-    vpAROgre ogre_;
-    vpCameraParameters cam_;
-    vpImage<vpRGBa> I_;
-    vpDisplayX display_;
-    bool init_, use_ar_;
+  vpAROgre ogre_;
+  vpCameraParameters cam_;
+  vpImage<vpRGBa> I_;
+  vpDisplayX display_;
+  bool init_, use_ar_;
 };
-
-
 
 #endif // AR_DISPLAY_H
